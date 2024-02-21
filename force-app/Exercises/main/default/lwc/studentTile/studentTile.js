b8a93bdd227c6db5eeb47ae85fe3e6a6200ccea6
DheerajBal <dheerajbal.18@gmail.com>
2024-02-21 05:23:57 +0000

    Exercise 2-9 Contact Selected

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -5,14 +5,20 @@ export default class StudentTile extends LightningElement {
         Name: 'Dheeraj',
         PhotoUrl: '/services/images/photo/003B0FakePictId',
     }
-    @api isSelected = false;
+    // @api isSelected = false;
 
     get tileSelected() {
-        return this.isSelected ? "tile selected" : "tile";
-    }
+        return (this.selectedStudentId===this.student.Id) ?
+        "tile selected" : "tile";
+        }
 
     studentClick(){
-        alert(this.student.Name);
-    }
+        const evt = new CustomEvent('studentselected', {
+        detail: { studentId: this.student.Id }
+        });
+        this.dispatchEvent(evt) ;
+        }
+    
+    @api selectedStudentId = '';
 
 }
