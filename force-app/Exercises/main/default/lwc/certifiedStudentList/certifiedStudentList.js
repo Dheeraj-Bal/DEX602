ac76b3796db028e52290a896c9974b177f234028
DheerajBal <dheerajbal.18@gmail.com>
2024-02-22 04:43:46 +0000

    Exercise 4-3 Completed

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import getCertifiedStudents from '@salesforce/apex/CertifiedStudentList.getCertifiedStudents';
+import deleteStudentCertification from '@salesforce/apex/CertifiedStudentList.deleteStudentCertification';
+import { refreshApex } from '@salesforce/apex';
 
 export default class CertifiedStudentList extends LightningElement {
     @api certificationId = 0;
@@ -7,9 +9,18 @@ export default class CertifiedStudentList extends LightningElement {
     btnGroupDisabled = true;
     certifiedStudents;
     error;
+    _wiredStudentResult;
+
+    // @wire(getCertifiedStudents, {certificationId:
+    //     '$certificationId'})
+    //     wired_getCertifiedStudents(result) {
+    //     this._wiredStudentResult = result;
+    //     this.certifiedStudents = [];
+    //     }
 
     @wire(getCertifiedStudents, {certificationId: '$certificationId'})
     wired_getCertifiedStudents(result) {
+        this._wiredStudentResult = result;
         this.certifiedStudents = [];
         if (result.data) {
         this.certifiedStudents = result.data.map(certHeld =>
@@ -49,5 +60,41 @@ export default class CertifiedStudentList extends LightningElement {
             const numSelected = event.detail.selectedRows.length;
             this.btnGroupDisabled = (numSelected === 0);
             }
+
+            getSelectedIDs() {
+                const datatable = this.template.querySelector('lightning-datatable');
+                const ids = datatable.getSelectedRows().map( (r) => (
+                    r.certificationHeldId
+                    ));
+                    return ids;
+            }
+
+            onCertActions (event) {
+                const btnClicked = event.target.getAttribute('data-btn-id');
+                switch (btnClicked) {
+                    case 'btnEmail':
+                        break;
+                    case 'btnSendCert':
+                        break;
+                    case 'btnDelete':
+                        this.onDelete();
+                        break;
+                    default:
+                        break;
+                    }
+            }
+
+            onDelete() {
+                const certificationIds = this.getSelectedIDs();
+                deleteStudentCertification({certificationIds})
+                .then( () => {
+                refreshApex(this._wiredStudentResult);
+                })
+                .catch(error => {
+                this.error = error;
+                });
+                }
+
+                
 
 }
